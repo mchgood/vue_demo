741e5e9416905cb5ffc9893d3727d4cbd099bd50
tangkc <1016771049@qq.com>
2019-09-15 21:15:07 +0800

    hello world

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img src="./assets/logo.png" height="200" width="200"/>
+    <!--  3.使用组件标签  -->
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+  // 1. 引入组件
+  import HelloWorld from './components/HelloWorld.vue'
+
+  export default {
+    //2. 映射组件标签
+    components:{
+      HelloWorld
+    }
+  }
+</script>
+
+<style>
+
+</style>
