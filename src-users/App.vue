2ed882b6200e1de8b2361a08d603f1164548689a
tangkc <1016771049@qq.com>
2019-09-21 14:37:22 +0800

    任务案例

---
+++ src-users/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <Search/>
+    <UserMain/>
+  </div>
+</template>
+
+<script>
+  import Search from './components/Search'
+  import Main from  './components/Main'
+
+  export default {
+    components:{
+      Search,
+      UserMain:Main //因为Main标签是html的关键字，所以需要别名
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
